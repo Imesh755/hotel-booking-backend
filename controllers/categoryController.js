a7a9922d8d9d0c6781dc78c0c79e61dacb2062e5
Imesh755 <imeshmihiranga755@gmail.com>
2024-12-06 11:03:02 +0530

    Routes of Category

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,1 +1,41 @@
 import Category from "../models/category.js";
+
+export function createCategory(req,res){
+    
+    if(req.user == null){
+        res.status(404).json({
+            message:"Unauthrized"
+        })
+        return
+    }
+    if(req.user.type !="admin"){
+        res.status().json({
+             message : "Frobidden"
+        })
+        return
+    }
+
+
+    const newCategory = new Category(req.body)
+    newCategory.save().then(
+        (result)=>{
+            res.json({
+                message:"Category created successfully"
+            })
+        }
+    ).catch(
+        (err)=>{
+            res.json(
+                {
+                    message:"Category creation failed",
+                    error :err
+                }
+            )
+        }
+    )
+
+
+
+
+
+}
